6e5362ac1de28f44b3905dbb72eac798d263102f
Kieu Binh <binhkt.vnu@gmail.com>
2019-04-05 13:24:03 +0700

    add importer to run on the server

---
+++ ES_importer.py
@@ -0,0 +1,7 @@
+
+from lib.elasticsearch.esimporter import ElasticsearchImporter
+from lib.constantvalues import ConstantValues
+if __name__ == '__main__':
+    # ElasticsearchImporter().jsonParser(corpusPath="../../data/acl/", index="acltest3",
+    #                                    doctype=ConstantValues.ACL_CORPUS_DOCTYPE)
+    ElasticsearchImporter().jsonParser(corpusPath="data/acl/", index=ConstantValues.ACL_CORPUS_INDEX, doctype=ConstantValues.ACL_CORPUS_DOCTYPE)
